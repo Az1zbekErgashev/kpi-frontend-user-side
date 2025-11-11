8642f717aa09bba74f6d51a97ade76699cc2fab6
ryanrepos <mamarasulov911@gmail.com>
2025-11-11 21:39:42 +0900

    fixed some bugs

--- src/components/Sitebar/index.tsx
+++ src/components/Sitebar/index.tsx
@@ -1,5 +1,5 @@
 import { ChevronDown, Zap } from 'lucide-react';
-import { NavLink, useLocation } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import { StyledSitebar } from './style';
 import { ADMIN_NAVIGATE } from 'utils/consts';
 import { useTranslation } from 'react-i18next';
@@ -26,7 +26,8 @@ export function Sitebar() {
         <div className="sidebar-background"></div>
         <div className="sidebar-content">
           {/* Logo Section */}
-          <div className="logo-section">
+          <Link to="/" className="sidebar-logo">
+            <div className="logo-section">
             <div className="logo-container">
               <div className="logo-icon">
                 <Zap className="logo-svg" />
@@ -38,6 +39,7 @@ export function Sitebar() {
             </div>
             <div className="logo-glow"></div>
           </div>
+          </Link>
 
           {/* Navigation */}
           <nav className="navigation">
